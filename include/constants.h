bb522d076371d3468afda0f8d8e682110b5fec4d
rnakai <rnakai@student.42tokyo.jp>
2021-01-27 15:20:17 +0900

    定数の値を定義したconstants.hの宣言

---
+++ include/constants.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANTS_H
+# define CONSTANTS_H
+
+# define STD_IN 0
+# define STD_OUT 1
+# define STD_ERR 2
+
+# define TRUE 1
+# define FALSE 0
+
+# define ERROR -1
+
+# define BUFFER_SIZE 1000
+
+#endif
