7333c4795bd41dd55510b101015a46ba2703cb8b
rnakai <rnakai@student.42tokyo.jp>
2021-01-30 00:01:30 +0900

    各々のファイル内でmainを書いて実行できるようにmakefileを改良。

--- src/main.c
+++ src/main.c
@@ -1,10 +1,15 @@
+#include "read_cmd_line.h"
+
+#ifndef DEBUG
 
 int		main(int argc, char **argv, char **envp)
 {
+	char	line[ARG_MAX];
+
 	setup_signal();
 	while (1)
 	{
-		read_line();
+		read_cmd_line(line);
 		parse_line();
 		create_empty_file(); // open とclose
 		exec_cmd();
@@ -12,4 +17,7 @@ int		main(int argc, char **argv, char **envp)
 		free_line();
 		free_list();
 	}
+	(void)argc; (void)argv; (void)envp;
 }
+
+#endif
