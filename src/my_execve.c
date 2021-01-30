325b0e993295a16b099c9007f69a0a058e445234
rnakai <rnakai@student.42tokyo.jp>
2021-01-30 11:58:22 +0900

    最低限コンパイルできるようにmy_execveを調整

--- src/my_execve.c
+++ src/my_execve.c
@@ -1,11 +1,13 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
-#include "utils.h"
+// #include "utils.h"
 #include "libft.h"
 #include "constants.h"
 #include "struct/data_list.h"
 #include "struct/env_list.h"
+#include <stdlib.h>
+#include "env_ctrl.h"
 
 t_env_list	*g_env_list;
 
@@ -20,13 +22,13 @@ static int is_there_execute_file_at(char *command_path)
 void		my_execve(t_cmd_list *cmd)
 {
 	int		i;
-	int		fd;
-	char	**arguments;	
+	// char	**arguments;	
 	char	**path_db_ptr;
 	char	*command_path;
+	char	*haribote[] = {"haribote", NULL};
 
-	if (is_there_execute_file_at(cmd->arg[0])){
-		if(execve(command_path, arguments, g_env_list) == ERROR)
+	if (is_there_execute_file_at(cmd->arg)){
+		if(execve(cmd->arg, haribote, haribote) == ERROR)
 		{
 			ft_perror("minishell");
 			exit(1);
@@ -37,18 +39,18 @@ void		my_execve(t_cmd_list *cmd)
 	path_db_ptr = get_path_array();	// TODO:free
 	while (path_db_ptr[i])
 	{
-		command_path = ft_strjoin(path_db_ptr[i], cmd->arg[0]);	// TODO:エラー処理
+		command_path = ft_strjoin(path_db_ptr[i], cmd->arg);	// TODO:エラー処理
 		//2)prefixをつけてファイルが存在するか（openできるか）検索
 		if (is_there_execute_file_at(command_path))
 		{
 			//3)ファイルが見つかったらprefixをつけてarguments二次元ポインタに格納する
-			if (execve(command_path, arguments, g_env_list) == ERROR)
+			if (execve(command_path, haribote, haribote) == ERROR)
 			{
 				ft_perror("minishell");
 				exit(1); //TODO: exitの値をどうするか要検証
 			}
 		}
 		ft_perror("minishell");
-		exit(1) //TODO: exitの値をどうするか要検証
+		exit(1); //TODO: exitの値をどうするか要検証
 	}
 }
