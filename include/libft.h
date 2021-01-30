055097997934c6ccc16fb87b16f4e162ff1e1cc1
yyabumot <yyabumot@student.42tokyo.jp>
2021-01-30 09:06:16 +0900

    libftにft_putendl_fdを追加

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: yyabumot <yyabumot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 11:36:34 by rnakai            #+#    #+#             */
-/*   Updated: 2021/01/29 17:11:06 by yyabumot         ###   ########.fr       */
+/*   Updated: 2021/01/30 09:05:34 by yyabumot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ void			*ft_calloc(size_t nmemb, size_t size);
 char			*ft_strjoin(char const *s1, char const *s2);
 void			ft_putchar_fd(char ch, int fd);
 void			ft_putstr_fd(char *str, int fd);
+void			ft_putendl_fd(char *s, int fd);
 void			ft_putnbr_fd(int num, int fd);
 char			**ft_split(char const *s, char c);
 
