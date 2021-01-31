fdfb746b39d8a4a66274ad85929d3619251bee01
yyabumot <yyabumot@student.42tokyo.jp>
2021-01-31 13:00:57 +0900

    複数の引数に対応

--- src/builtins/my_echo.c
+++ src/builtins/my_echo.c
@@ -4,9 +4,17 @@
 // -nオプション
 // 有->endl_flag = 1
 // 無->endl_flag = 0
-void my_echo(char *arg, int endl_flag)
+void my_echo(char **args, int endl_flag)
 {
-	ft_putstr_fd(arg, STD_OUT);
+	int	i;
+
+	i = 0;
+	while(args[i])
+	{
+		ft_putstr_fd(args[i], STD_OUT);
+		ft_putchar_fd(' ', STD_OUT);
+		++i;
+	}
 	if(!endl_flag)
 		ft_putstr_fd("\n", STD_OUT);
 }
@@ -15,7 +23,7 @@ void my_echo(char *arg, int endl_flag)
 
 int main(void)
 {
-	char *arg = "hello world";
+	char *arg []= {"hello", "world", NULL};
 
 	my_echo(arg, 0);
 	my_echo(arg, 1);
