e1b8dc1522b7949da412f5264165f41e9db70df5
yyabumot <yyabumot@student.42tokyo.jp>
2021-01-31 21:27:39 +0900

    ファイル内の整形

--- src/builtins/my_echo.c
+++ src/builtins/my_echo.c
@@ -36,11 +36,11 @@ void set_idx_and_endline_flag(char **args, int *idx, int *endline_flag)
 	}
 }
 
-#include <stdio.h>
 void my_echo(char **args)
 {
 	int idx;
 	int endline_flag;	// 行末に改行を入れるかどうかのフラグ
+
 
 	if(!is_valid_argument(args))
 	{
@@ -61,37 +61,37 @@ void my_echo(char **args)
 
 int main(void)
 {
-	// char *args[] = {"echo", "hello1", NULL};
-	// char *args2[] = {"echo", "-nn", NULL};
-	// char *args3[] = {"echo", "-nnnnnnn", NULL};
-	// char *args4[] = {"echo", "-nnnnnnn", "hello", NULL};
-	// char *args5[] = {"echo", "-n", "-n", "-n", NULL};
-	// char *args6[] = {"echo", "-n", "-n", "6", "6", NULL};
-	// char *args7[] = {"echo", "-n4", NULL};
-	// char *args8[] = {"echo", "-n!", NULL};
-	// char *args9[] = {"echo", "-n", NULL};
-	// char *args10[] = {"echo", "-n", "world10", NULL};
-	// char *args11[] = {"echo", "-n", "hello11", "world", NULL};
+	char *args[]= {"echo", "hello1", NULL};
+	char *args2[] = {"echo", "-nn", NULL};
+	char *args3[] = {"echo", "-nnnnnnn", NULL};
+	char *args4[] = {"echo", "-nnnnnnn", "hello", NULL};
+	char *args5[] = {"echo", "-n", "-n", "-n", NULL};
+	char *args6[] = {"echo", "-n", "-n", "6", "6", NULL};
+	char *args7[] = {"echo", "-n4", NULL};
+	char *args8[] = {"echo", "-n!", NULL};
+	char *args9[] = {"echo", "-n", NULL};
+	char *args10[] = {"echo", "-n", "world10", NULL};
+	char *args11[] = {"echo", "-n", "hello11", "world", NULL};
 
 	char *args12[] = {"echo", "hoge", "\n", NULL};
-	// char *args13[] = {"echo", "hoge", "fuga", NULL};
+	char *args13[] = {"echo", "hoge", "fuga", NULL};
 
-	// char *args14[] = {"echo", NULL};
+	char *args14[] = {"echo", NULL};
 
-	// my_echo(args);	//ok
-	// my_echo(args2);
-	// my_echo(args3);
-	// my_echo(args4);
-	// my_echo(args5);
-	// my_echo(args6);
-	// my_echo(args7);
-	// my_echo(args8);
-	// my_echo(args9);
-	// my_echo(args10);
-	// my_echo(args11);
+	my_echo(args);
+	my_echo(args2);
+	my_echo(args3);
+	my_echo(args4);
+	my_echo(args5);
+	my_echo(args6);
+	my_echo(args7);
+	my_echo(args8);
+	my_echo(args9);
+	my_echo(args10);
+	my_echo(args11);
 	my_echo(args12);
-	// my_echo(args13);
-	// my_echo(args14);	//ok
+	my_echo(args13);
+	my_echo(args14);
 }
 
 #endif
