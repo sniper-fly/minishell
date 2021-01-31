ba79420b1034e914b088150b01a277b6c9dc929c
yyabumot <yyabumot@student.42tokyo.jp>
2021-01-31 10:53:18 +0900

    make_new_env_node関数をmake_env_list.cから削除

--- src/env/make_env_list.c
+++ src/env/make_env_list.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
 #include "libft.h"
 #include "struct/env_list.h"
-#include <stdlib.h>
 
 static void init_env_list(void)
 {
@@ -10,19 +11,6 @@ static void init_env_list(void)
 	g_env_list->value = NULL;
 	g_env_list->prev = NULL;
 	g_env_list->next = NULL;
-}
-
-t_env_list *make_new_env_node(char *envp_i)
-{
-	t_env_list *new;
-	char **splitted;
-
-	new = malloc(sizeof(t_env_list));	// TODO:mallocエラー処理
-	splitted = ft_split(envp_i, '=');	// TODO:エラー処理
-	new->key = splitted[0];
-	new->value = splitted[1];
-	free(splitted);
-	return new;
 }
 
 void make_env_list(char **envp)
