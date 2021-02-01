5c47a22b582271359945e0f1f084941beb4374d2
rnakai <rnakai@student.42tokyo.jp>
2021-02-01 17:03:27 +0900

    型検査を強化するため、TRUE, FALSEのみを持つt_bool型を定義した

--- include/constants.h
+++ include/constants.h
@@ -1,15 +1,18 @@
 #ifndef CONSTANTS_H
 # define CONSTANTS_H
 
+/*
+** define true and false below
+*/
+
+# include "struct/t_bool.h"
+
 # define STD_IN 0
 # define STD_OUT 1
 # define STD_ERR 2
 
-# define TRUE 1
-# define FALSE 0
-
 # define ERROR -1
 
-# define BUFFER_SIZE 1000
+# define ARG_MAX 262144 //guacamolle でgetconf ARG_MAX
 
 #endif
