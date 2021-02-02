f85b436507e5b1d7f84c5a1e4b4ca747a7be6575
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-02 15:02:15 +0900

    環境変数処理系関数のプロトタイプ宣言をenv_ctrl.hに追加

--- include/env_ctrl.h
+++ include/env_ctrl.h
@@ -1,6 +1,10 @@
 #ifndef ENV_CTRL_H
 #define ENV_CTRL_H
 
+#include "struct/env_list.h"
+
 char **get_path_array(void);
+void create_env_list(char **envp);
+t_env_list *make_new_env_node(char *raw_env_var);
 
 #endif
