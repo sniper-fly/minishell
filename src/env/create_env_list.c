f8135dad2c6faf907a786277496d5fd0de47b77b
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-06 09:20:01 +0900

    create_env_list関数にテスト用のmain関数を追加

--- src/env/create_env_list.c
+++ src/env/create_env_list.c
@@ -36,12 +36,21 @@ void create_env_list(char **envp)
 	}
 }
 
-#ifdef MAKE_ENV_LIST_C
+#ifdef CREATE_ENV_LIST_C
 
-int	main(void)
+int	main(int argc, char *argv[], char *envp[])
 {
-	;
-	//none test yet
+	t_env_list *env_node;
+
+	argc += 1;
+	argv[0][0] = 'a';
+	create_env_list(envp);
+	env_node = g_env_list->next;
+	while(env_node != g_env_list)
+	{
+		printf("%s=%s\n", env_node->key, env_node->value);
+		env_node = env_node->next;
+	}
 }
 
 #endif
