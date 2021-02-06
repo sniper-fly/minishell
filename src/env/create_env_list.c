91016d9262c7f6dacc19b950b2eca0d5e14af650
rnakai <rnakai@student.42tokyo.jp>
2021-02-06 18:16:06 +0900

    env_listの番兵の修正

--- src/env/create_env_list.c
+++ src/env/create_env_list.c
@@ -9,8 +9,8 @@ static void init_env_list(void)
 	g_env_list = malloc(sizeof(t_env_list));	// TODO:mallocエラー処理
 	g_env_list->key = NULL;
 	g_env_list->value = NULL;
-	g_env_list->prev = NULL;
-	g_env_list->next = NULL;
+	g_env_list->prev = g_env_list;
+	g_env_list->next = g_env_list;
 }
 
 void create_env_list(char **envp)
