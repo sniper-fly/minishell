f4e88ce515d544850e7442d16229da59cb45b3ac
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-08 07:58:10 +0900

    エラー処理の追加，関数の分割

--- src/exec_cmd/my_execve.c
+++ src/exec_cmd/my_execve.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include "utils.h"
 #include "libft.h"
+#include "exec_cmd.h"
 #include "env_ctrl.h"
 #include "constants.h"
 #include "struct/process.h"
@@ -12,82 +13,87 @@
 
 t_env_list	*g_env_list;
 
-static int is_there_execute_file_at(char *command_path)
+static int is_there_execute_file_at(char *cmd_path)
 {
 	int		fd;
 
-	fd = open(command_path, O_WRONLY);
+	fd = open(cmd_path, O_WRONLY);
 	return (fd != ERROR || errno != ENOENT);
 }
 
-static char *get_command_path(char *cmd, char *path_ptr)
+static void check_if_the_full_path_is_valid(char *cmd_path)
 {
-	char *add_slash;
-	char *command_path;
-
-	add_slash = NULL;
-	if(path_ptr[ft_strlen(path_ptr) - 1] != '/')
+	if (open(cmd_path, O_RDWR) == ERROR && (errno == EISDIR || errno == ENOENT))
 	{
-		add_slash = ft_strjoin(path_ptr, "/");
-		command_path = ft_strjoin(add_slash, cmd);	// TODO:エラー処理
-	}
-	else
-		command_path = ft_strjoin(path_ptr, cmd);
-	free(add_slash);
-	return (command_path);
-}
-
-static void occur_an_error_at_execve(void)
-{
-	ft_perror("minishell");
-	exit(1);	// TODO: exitのステータス要検証
-}
-
-static void check_if_the_full_path_is_valid(char *path)
-{
-	if(open(path, O_RDWR) == -1 && (errno == EISDIR || errno == ENOENT)){
 		ft_putstr_fd("minishlell: ", STD_ERR);
-		ft_putstr_fd(path, STD_ERR);
+		ft_putstr_fd(cmd_path, STD_ERR);
 		ft_putstr_fd(": ", STD_ERR);
 		ft_putendl_fd(strerror(errno), STD_ERR);
 		exit(1);
 	}
 }
 
+static void if_command_not_found(char *cmd_path)
+{
+	ft_putstr_fd(cmd_path, STD_ERR);
+	ft_putstr_fd(": ", STD_ERR);
+	ft_putendl_fd("command not found", STD_ERR);
+	exit(1);
+}
+
+static char *get_command_path(char *cmd, char *path_ptr)
+{
+	char *add_slash;
+	char *cmd_path;
+
+	add_slash = NULL;
+	if(path_ptr[ft_strlen(path_ptr) - 1] != '/')
+	{
+		add_slash = ft_strjoin(path_ptr, "/");
+		cmd_path = ft_strjoin(add_slash, cmd);	// TODO:エラー処理
+	}
+	else
+		cmd_path = ft_strjoin(path_ptr, cmd);
+	free(add_slash);
+	return (cmd_path);
+}
+
+static void do_execve(char *cmd_path, char **cmd, char **envp)
+{
+	// cmd[0] = cmd_path; // TODO:cmd[0]に代入必要?
+	if (execve(cmd_path, cmd, envp) == ERROR)
+	{
+		ft_perror("minishell");
+		exit(1);	// TODO: exitのステータス要検証
+	}
+}
+
 void		my_execve(char **cmd)
 {
 	int		i;
-	char	**path_db_ptr;
+	char	*cmd_path;
 	char	**envp;
-	char	*command_path;
+	char	**path_db_ptr;
 
 	envp = get_env_array();
 	if(cmd[0][0] == '/')
 		check_if_the_full_path_is_valid(cmd[0]);
-	if (is_there_execute_file_at(cmd[0])){
-		if(execve(cmd[0], cmd, envp) == ERROR)
-			occur_an_error_at_execve();
-	}
+	if (is_there_execute_file_at(cmd[0]))
+		do_execve(cmd[0], cmd, envp);
 	//1)envlstからPATHを取得し、PATHをsplitして二次元配列を作る。
 	i = 0;
 	path_db_ptr = get_path_array();	// TODO:free?
 	while (path_db_ptr[i])
 	{
-		command_path = get_command_path(cmd[0], path_db_ptr[i]);
+		cmd_path = get_command_path(cmd[0], path_db_ptr[i]);
 		//2)prefixをつけてファイルが存在するか（openできるか）検索
-		if (is_there_execute_file_at(command_path))
-		{
-			//3)ファイルが見つかったらprefixをつけてarguments二次元ポインタに格納する
-			if (execve(command_path, cmd, envp) == ERROR)
-				occur_an_error_at_execve();
-		}
-		free(command_path);
+		//3)ファイルが見つかったらprefixをつけてarguments二次元ポインタに格納する
+		if (is_there_execute_file_at(cmd_path))
+			do_execve(cmd_path, cmd, envp);
+		free(cmd_path);
 		++i;
 	}
-	ft_putstr_fd(cmd[0], STD_ERR);
-	ft_putstr_fd(": ", STD_ERR);
-	ft_putendl_fd("command not found", STD_ERR);
-	exit(1);
+	if_command_not_found(cmd[0]);
 }
 
 #ifdef MY_EXECVE_C
@@ -108,7 +114,7 @@ int main(int argc, char *argv[], char *envp[])
 {
 	char *cmd[] = {"ls", "-l", NULL};
 	
-	char *cmd1[] = {"/bin/", "-l", NULL};
+	char *cmd1[] = {"/bin/ls", "-l", NULL};
 	// ディレクトリを指定
 
 	char *cmd2[] = {"/bin/", "-l", NULL};
