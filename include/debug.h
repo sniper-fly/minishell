554a7195d1c50e01dd1750a4908a47097eaaa3f2
rnakai <rnakai@student.42tokyo.jp>
2021-02-09 23:50:01 +0900

    ヘッダーに足りない宣言を補足した

--- include/debug.h
+++ include/debug.h
@@ -6,5 +6,6 @@ void		show_string_arr(char **strings);
 void		show_procs(t_process **procs);
 t_process	**generate_simple_procs(char *line);
 void		show_str_procs(char ***str_procs);
+void		free_procs(t_process **procs);
 
 #endif
