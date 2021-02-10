3cd3f96c6ba18e8107cabf1ce19443287944e152
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-10 17:15:08 +0900

    exit_status.hにstatusの追加

--- include/exit_status.h
+++ include/exit_status.h
@@ -3,9 +3,10 @@
 
 #define SUCCESSED 0
 #define FAILED 1
+#define IS_DIR 126
 #define PERMISSION_DENIED 126
 #define COMMAND_NOT_FOUND 127
-#define INVALID_VALUE_TO_EXIT_CMD 128	// exitに不正な値(浮動小数点など)を渡した
+// #define INVALID_VALUE_TO_EXIT_CMD 128	// exitに不正な値(浮動小数点など)を渡した
 #define CTRL_C 130
 #define CTRL_BKSLSH 131
 
