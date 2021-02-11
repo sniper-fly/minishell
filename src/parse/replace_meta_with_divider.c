3a60b27b31d4780682380d0578656f34b4a8d249
rnakai <rnakai@student.42tokyo.jp>
2021-02-11 20:09:46 +0900

    convert_line関数でダブルクォートの中にバックスラッシュ+ダブルクォートがあったときそこでクォートが切れてしまうバグを直した

--- src/parse/replace_meta_with_divider.c
+++ src/parse/replace_meta_with_divider.c
@@ -1,14 +1,29 @@
 #include "parse.h"
+#include "constants.h"
 
-static void	skip_until_end_quote(char *line, int *idx)
+static void	skip_until_end_single_quote(char *line, int *idx)
 {
-	char	quote;
-
-	quote = line[*idx];
 	(*idx)++;
 	//ヌル文字までにクォートが閉じているかはvalidateで検査するが、一応チェックしておく
-	while (line[*idx] != quote && line[*idx] != '\0')
+	while (line[*idx] != SINGLE_QUOTE && line[*idx] != '\0')
 		(*idx)++;
+	(*idx)++;
+}
+
+static void	skip_until_end_double_quote(char *line, int *idx)
+{
+	(*idx)++;
+	while (1)
+	{
+		if (line[*idx] == BACK_SLASH)
+		{
+			(*idx) += 2;
+			continue ;
+		}
+		if (line[*idx] == DOUBLE_QUOTE || line[*idx] == '\0')
+			break;
+		(*idx)++;
+	}
 	(*idx)++;
 }
 
@@ -19,12 +34,17 @@ void		replace_meta_with_divider(char *line, char rplced_ch)
 	i = 0;
 	while (line[i])
 	{
-		if (line[i] == '\"' || line[i] == '\'')
+		if (line[i] == SINGLE_QUOTE)
 		{
-			skip_until_end_quote(line, &i);
+			skip_until_end_single_quote(line, &i);
 			continue ;
 		}
-		else if (line[i] == '\\' && line[i + 1] == rplced_ch)
+		else if (line[i] == DOUBLE_QUOTE)
+		{
+			skip_until_end_double_quote(line, &i);
+			continue ;
+		}
+		else if (line[i] == BACK_SLASH && line[i + 1] == rplced_ch)
 		{
 			i += 2; //ヌル終端は保証されているのでセグフォしない
 			continue ;
