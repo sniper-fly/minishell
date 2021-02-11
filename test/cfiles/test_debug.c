ae46be58ad6ac135262ad58c403de570d99e0927
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-11 11:41:04 +0900

    test_debug.cにexec_cmd_for_debug関数のテストを追加

--- test/cfiles/test_debug.c
+++ test/cfiles/test_debug.c
@@ -58,3 +58,35 @@ int		main(void)
 }
 
 #endif
+
+#ifdef EXEC_CMD_FOR_DEBUG_C
+
+#include <sys/wait.h>
+#include "execute.h"
+#include "main.h"
+#include "parse.h"
+#include "utils.h"
+#include "debug.h"
+#include "constants.h"
+#include "read_cmd_line.h"
+#include "libft.h"
+
+int g_status;
+
+int main(void)
+{
+	char cmd_line[ARG_MAX + 1];
+	t_process **cmd_procs;
+
+	while(TRUE)
+	{
+		print_prompt();
+		read_cmd_line(cmd_line);
+		cmd_procs = generate_simple_procs(cmd_line);
+		exec_cmd_for_debug(cmd_procs);
+		ft_putnbr_fd(WEXITSTATUS(g_status), 1);
+		pendl();
+	}
+}
+
+#endif
