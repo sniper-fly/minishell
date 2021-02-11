7eac1c51ea4ed3e85ba521e250ccda1d568e73a0
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-11 11:12:26 +0900

    デバッグ用exec_cmd関数の追加

---
+++ src/debug/exec_cmd_for_debug.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include "execute.h"
+#include "struct/process.h"
+
+extern int	g_status;
+
+void	exec_cmd_for_debug(t_process **procs)
+{
+	int i;
+
+	i = 0;
+	while (procs[i]) //行のループ
+	{
+		// TODO:ビルトインを実行するかどうかのチェック(前後が番兵かi.e.コマンドが単一かどうか)
+		exec_pipes(procs[i]); //パイプのwhileループ(列のループ)
+		++i;
+	}
+}
