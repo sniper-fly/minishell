155c8e1f1e3ee33aa3c858f06bebdc11e492d8bc
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-12 16:24:04 +0900

    コマンドラインのメタ文字やリダイレクトが正当か判定するis_valid_meta...関数を追加

---
+++ src/validation/is_valid_meta_and_redirect.c
@@ -0,0 +1,80 @@
+#include <stdlib.h>
+#include "validation.h"
+#include "struct/t_bool.h"
+
+static void skip_space(char **cmd_line)
+{
+	while(is_space(**cmd_line))
+		++(*cmd_line);
+}
+
+static void skip_normal_char(char **cmd_line)
+{
+	// null文字でない かつ ((メタ文字でない かつ リダイレクトでない) または 空白)
+	// (メタ文字でない かつ リダイレクトでない)の条件でド・モルガンの法則を使用
+	while(**cmd_line
+	&& (!(is_meta_char(**cmd_line) || is_redirect_char(**cmd_line)) || is_space(**cmd_line)))
+		++(*cmd_line);
+}
+
+static t_bool is_valid_redirect(char **cmd_line)
+{
+	if(**cmd_line == '<')
+		++(*cmd_line);
+	else if (**cmd_line == '>')
+	{
+		++(*cmd_line);
+		if(**cmd_line == '>')
+			++(*cmd_line);
+	}
+	skip_space(cmd_line);
+	// リダイレクト後リダイレクト
+	if(is_redirect_char(**cmd_line))	// TODO:エラー出力
+		return FALSE;
+	return TRUE;
+}
+
+static t_bool is_valid_meta(char **cmd_line, int redirect_flag)
+{
+	// リダイレクト後メタ
+	if(redirect_flag)	// TODO:エラー出力
+		return FALSE;
+	++(*cmd_line);
+	skip_space(cmd_line);
+	// メタ後メタ
+	if(is_meta_char(**cmd_line))	// TODO:エラー出力
+		return FALSE;
+	return TRUE;
+}
+
+t_bool is_valid_meta_and_redirect(char *cmd_line)
+{
+	int redirect_flag;
+
+	skip_space(&cmd_line);
+	// 初手メタ
+	if(is_meta_char(*cmd_line) == TRUE)
+		return FALSE;
+	while(*cmd_line)
+	{
+		redirect_flag = 0;
+		// 関係ない文字をスキップ
+		skip_normal_char(&cmd_line);
+		if(is_redirect_char(*cmd_line))
+		{
+			redirect_flag = 1;
+			if(!is_valid_redirect(&cmd_line))
+				return FALSE;
+		}
+		if(is_meta_char(*cmd_line))
+		{
+			if(!is_valid_meta(&cmd_line, redirect_flag))
+				return FALSE;
+		}
+		skip_space(&cmd_line);
+		// 最後リダイレクト
+		if(redirect_flag && !(*cmd_line))
+			return FALSE;
+	}
+	return TRUE;
+}
