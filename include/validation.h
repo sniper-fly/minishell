3954e38ce207e67c35940e754a6f5d02db919b36
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-13 10:20:49 +0900

    ソースファイルの分割によるヘッダーの変更

--- include/validation.h
+++ include/validation.h
@@ -3,12 +3,13 @@
 
 #include "struct/t_bool.h"
 
-void put_syntax_error_message(char *msg);
-t_bool is_meta_char(char c);
-t_bool is_redirect_char(char c);
-t_bool is_space(char c);
-t_bool is_valid_char_code(char *cmd_line);
-
-t_bool is_valid_meta_and_redirect(char *cmd_line);
+void	put_syntax_error_message(char *msg);
+t_bool	is_meta_char(char c);
+t_bool	is_redirect_char(char c);
+void	skip_space(char **cmd_line);
+t_bool	is_valid_char_code(char *cmd_line);
+t_bool	is_valid_meta(char **cmd_line, t_bool is_redirect);
+t_bool	is_valid_redirect(char **cmd_line, t_bool *is_redirect);
+t_bool	is_valid_meta_and_redirect(char *cmd_line);
 
 #endif
