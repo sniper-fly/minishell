bdd4046b53f4d8381d8abf9d54b21bcc4c13a8d6
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-13 14:26:14 +0900

    validationを全部実行してくれる総括関数

---
+++ src/validation/is_valid_command_line.c
@@ -0,0 +1,13 @@
+#include "validation.h"
+#include "struct/t_bool.h"
+
+t_bool is_valid_command_line(char *cmd_line)
+{
+	if (!is_valid_char_code(cmd_line))
+		return (FALSE);
+	if (!is_valid_meta_and_redirect(cmd_line))
+		return (FALSE);
+	if (!is_valid_quote(cmd_line))
+		return (FALSE);
+	return (TRUE);
+}
