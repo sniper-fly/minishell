7bb9ca14151f1008064deda38aea8bd0c358e7f5
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-13 10:55:58 +0900

    search_env_node関数のテストを追加

--- test/cfiles/test_env.c
+++ test/cfiles/test_env.c
@@ -1,6 +1,44 @@
 #include "env_ctrl.h"
 #include "struct/env_list.h"
 
+#ifdef SEARCH_ENV_NODE_C
+
+#include "libft.h"
+#include "constants.h"
+
+t_env_list *g_env_list;
+
+void test_search_env_node(char *key)
+{
+	t_env_list *search_node;
+
+	search_node = search_env_node(key);
+	ft_putstr_fd(key, STD_OUT);
+	ft_putstr_fd(" : ", STD_OUT);
+	if(search_node == g_env_list)
+		ft_putendl_fd("\x1b[31mEnv not found\x1b[m", STD_OUT);
+	else
+		ft_putendl_fd(search_node->value, STD_OUT);
+}
+
+int main(int argc, char *argv[], char *envp[])
+{
+	char *env_keys[] = 
+	{
+		"PWD",
+		"SHELL",
+		"foo"
+	};
+
+	argc += 1;
+	argv[0][0] = 'a';
+	create_env_list(envp);
+	for(size_t i = 0; i < sizeof(env_keys) / sizeof(char *); ++i)
+		test_search_env_node(env_keys[i]);
+}
+
+#endif
+
 #ifdef CREATE_ENV_LIST_C
 
 int	main(int argc, char *argv[], char *envp[])
