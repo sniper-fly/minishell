c1a803358e24cffd33499596edf6c326004f643a
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-15 08:24:37 +0900

    my_exportのテストを追加

--- test/cfiles/test_builtins.c
+++ test/cfiles/test_builtins.c
@@ -1,4 +1,66 @@
-#include "builtins.h"
+#include "builtins/builtins.h"
+
+#ifdef MY_EXPORT_C
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
+#include "utils.h"
+#include "env_ctrl.h"
+#include "constants.h"
+#include "struct/env_list.h"
+
+void test_my_export(char **args)
+{
+	int i;
+	char **allocated_arg;
+	char *simple_export[] = {"export", NULL};
+
+	ft_putstr_fd("\n====================\n", STD_OUT);
+	allocated_arg = malloc(sizeof(args));
+	for(i = 0; args[i]; ++i)
+		allocated_arg[i] = ft_strdup(args[i]);
+	allocated_arg[i] = NULL;
+	my_export(allocated_arg);
+	// free_string_arr(allocated_arg);
+	my_export(simple_export);
+}
+
+int main(int argc, char *argv[], char *envp[])
+{
+	char *args1[] = {"export", "foo=foo1", NULL};
+	char *args2[] = {"export", "hoge=hoge", NULL};
+	char *args3[] = {"export", "foo=foo3", NULL};
+	char *args4[] = {"export", "foo=foo4$hoge", NULL};
+	char *args5[] = {"export", "foo=foo5", "hoge=hoge5", NULL};
+	char *args6[] = {"export", "foo=foo6$hoge", NULL};
+	char *args7[] = {"export", "hoge=hoge", "foo=$hoge:foo", NULL};
+
+	// $で指定した環境変数が存在しない
+	char *args10[] = {"export", "foo=foo7$ava$hoge", NULL};
+
+	char *args11[] = {"export", "ava=", NULL};
+	char *args12[] = {"export", "ava", NULL};
+
+
+	argc += 1;
+	argv[0][0] = 'a';
+	create_env_list(envp);
+
+	test_my_export(args1);
+	test_my_export(args2);
+	test_my_export(args3);
+	test_my_export(args4);
+	test_my_export(args5);
+	test_my_export(args6);
+	test_my_export(args7);
+
+	test_my_export(args10);
+	test_my_export(args11);
+	test_my_export(args12);
+}
+
+#endif
 
 #ifdef MY_ECHO_C 
 
