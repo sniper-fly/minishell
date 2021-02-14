0e25904e893c39d505d68a8a533fc905bc159c3d
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-14 22:57:09 +0900

    =がない環境変数を追加するとき(e.g. export foo)の対処を追加

--- src/env/make_new_env_node.c
+++ src/env/make_new_env_node.c
@@ -12,9 +12,14 @@ t_env_list *make_new_env_node(char *raw_env_var)
 	new = malloc(sizeof(t_env_list));	// TODO:mallocエラー処理
 	key_ptr = raw_env_var;
 	equal_ptr = ft_strchr(raw_env_var, '=');	// TODO:エラー処理
-	*equal_ptr = '\0';
-	val_ptr = equal_ptr + 1;
-	new->key = ft_strdup(key_ptr);	// TODO?エラー処理
-	new->value = ft_strdup(val_ptr);	// TODO:エラー処理
+	if(equal_ptr)
+	{
+		*equal_ptr = '\0';
+		val_ptr = equal_ptr + 1;
+		new->key = ft_strdup(key_ptr);	// TODO?エラー処理
+		new->value = ft_strdup(val_ptr);	// TODO:エラー処理
+	}
+	else
+		new->key = NULL;
 	return new;
 }
