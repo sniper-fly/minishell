0e9265f059141fb3925641cd654cbca6ba41a164
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-15 08:22:56 +0900

    make_new_env_node関数の整形

--- src/env/make_new_env_node.c
+++ src/env/make_new_env_node.c
@@ -4,10 +4,10 @@
 
 t_env_list *make_new_env_node(char *raw_env_var)
 {
-	t_env_list *new;
-	char *equal_ptr;
-	char *key_ptr;
-	char *val_ptr;
+	char		*equal_ptr;
+	char		*key_ptr;
+	char		*val_ptr;
+	t_env_list	*new;
 
 	new = malloc(sizeof(t_env_list));	// TODO:mallocエラー処理
 	key_ptr = raw_env_var;
@@ -16,13 +16,10 @@ t_env_list *make_new_env_node(char *raw_env_var)
 	{
 		*equal_ptr = '\0';
 		val_ptr = equal_ptr + 1;
-		new->key = ft_strdup(key_ptr);	// TODO?エラー処理
 		new->value = ft_strdup(val_ptr);	// TODO:エラー処理
 	}
 	else
-	{
-		new->key = ft_strdup(key_ptr);
 		new->value = NULL;
-	}
+	new->key = ft_strdup(key_ptr);	// TODO?エラー処理
 	return new;
 }
