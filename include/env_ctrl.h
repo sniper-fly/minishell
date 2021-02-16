dab7cedb8c0ee8fd29646b7942d547bcc237178f
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-16 18:07:35 +0900

    is_valid_env_keyをmake_new_env_node関数から外に出した

--- include/env_ctrl.h
+++ include/env_ctrl.h
@@ -1,6 +1,7 @@
 #ifndef ENV_CTRL_H
 #define ENV_CTRL_H
 
+#include "struct/t_bool.h"
 #include "struct/env_list.h"
 
 char **get_path_array(void);
@@ -9,5 +10,7 @@ void create_env_list(char **envp);
 t_env_list *make_new_env_node(char *raw_env_var);
 t_env_list *search_env_node(char *search_key);
 void free_env_node(t_env_list *env_node);
+t_bool is_valid_env_key(char *key);
+
 
 #endif
