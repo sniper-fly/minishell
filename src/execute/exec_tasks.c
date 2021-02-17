7828f77fc7464893eec1ebbec14f0900801fd8af
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-17 10:10:23 +0900

    exec_cmdからexec_tasksに改名

---
+++ src/execute/exec_tasks.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include "execute.h"
+#include "struct/process.h"
+
+extern int	g_status;
+
+void	exec_tasks(char ***tasks)
+{
+	int i;
+	t_process *procs;
+
+	i = 0;
+	while (tasks[i]) //行のループ
+	{
+		procs = NULL;	// TODO:後で消す
+		// procs = parse(tasks[i]);
+		// TODO:procs = parse(tasks[i]);
+		// TODO:create_empty_file();
+		// TODO:ビルトインを実行するかどうかのチェック(前後が番兵かi.e.コマンドが単一かどうか)
+		exec_pipes(procs); //パイプのwhileループ(列のループ)
+		// TODO:free_procs(procs);
+		++i;
+	}
+}
