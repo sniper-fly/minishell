c42bbd23f31a4624dedbd6f39bbf9d53ff65101e
rnakai <rnakai@student.42tokyo.jp>
2021-02-17 23:55:53 +0900

    渡されたインデックスから環境変数の終端までの文字列をコピーする関数get_single_env_keyを作成

--- include/parse.h
+++ include/parse.h
@@ -9,6 +9,8 @@
 # define RESIZE_BUF_LEN 1000 
 #endif
 
+# define BUF_LEN 1000
+
 char		***convert_line2tasks(char *line);
 void		replace_meta_with_divider(char *line, char rplced_ch);
 void		free_tasks(char ***tasks);
@@ -16,5 +18,6 @@ t_process	*parse_each_task(char **str_procs);
 void		parse_redirect_and_fill_space(char *str_proc, t_process *proc);
 char		*expand_env_var_str(char *str);
 char		*auto_resize_cpy(char *dst, int idx, int *buf_size, char src_ch);
+char		*get_single_env_key(char *str, int idx);
 
 #endif
--- test/cfiles/test_parse.c
+++ test/cfiles/test_parse.c
@@ -1,4 +1,51 @@
 
+#ifdef GET_SINGLE_ENV_KEY_C
+#include "parse.h"
+#include <stdio.h>
+#include "libft.h"
+#include "constants.h"
+#include <stdlib.h>
+#include "utils.h"
+
+void	test_get_single_env_key(char *str, int idx)
+{
+	static int num = 1;
+	char	*tmp = get_single_env_key(str, idx);
+
+	ft_putnbr_fd(num, STD_OUT);
+	pendl();
+	ft_putendl_fd(tmp, STD_OUT);
+	ft_putendl_fd("=============", STD_OUT);
+	free(tmp);
+	num++;
+}
+
+int		main(void)
+{
+	char str1[] = "$world";
+	char str2[] = "$\"\"world";
+	char str3[] = "$world$hoge";
+	char str4[] = "$hey ";
+	char str5[] = "$hey\t ";
+	char str6[] = "a$hey\"";
+	char str7[] = "ab$hey\\";
+	char str8[] = "a$hey'";
+	char str9[] = "$$hey'";
+
+	test_get_single_env_key(str1, 1);
+	test_get_single_env_key(str2, 1);
+	test_get_single_env_key(str3, 1);
+	test_get_single_env_key(str4, 1);
+	test_get_single_env_key(str5, 1);
+	test_get_single_env_key(str6, 2);
+	test_get_single_env_key(str7, 3);
+	test_get_single_env_key(str8, 2);
+	test_get_single_env_key(str9, 2);
+	test_get_single_env_key(str9, 1);
+}
+
+#endif
+
 #ifdef AUTO_RESIZE_CPY_C
 #include <stdlib.h>
 #include "parse.h"
