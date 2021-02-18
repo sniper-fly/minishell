6531958d51cff324045feb68f5ca47b349078ef1
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-19 07:04:25 +0900

    exitステータスの修正

--- include/exit_status.h
+++ include/exit_status.h
@@ -2,11 +2,11 @@
 #define EXIT_STATUS_C
 
 #define SUCCEEDED 0
-#define FAILED 1
-#define IS_DIR 126
-#define PERMISSION_DENIED 126
-#define COMMAND_NOT_FOUND 127
-// #define INVALID_VALUE_TO_EXIT_CMD 128	// exitに不正な値(浮動小数点など)を渡した
+#define GENERAL_ERROROS 1
+#define MISUSE_OF_SHELL_BUILTINS 2	// Missing keyword or command, or permission problem
+#define CMD_CANNOT_EXECUTE 126	// Permission problem or command is not executable
+#define COMMAND_NOT_FOUND 127	// Possible problem with $PATH or a typo
+// #define INVALID_ARG_TO_EXIT 128	// exitに不正な値(浮動小数点など)を渡した
 #define CTRL_C 130
 #define CTRL_BKSLSH 131
 
