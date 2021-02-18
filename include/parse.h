b050a5138f8fa74168eed209218d4f1658d52045
rnakai <rnakai@student.42tokyo.jp>
2021-02-18 17:50:48 +0900

    dstの末尾からコピーするauto_resize_join関数の作成

--- include/parse.h
+++ include/parse.h
@@ -19,5 +19,7 @@ void		parse_redirect_and_fill_space(char *str_proc, t_process *proc);
 char		*expand_env_var_str(char *str);
 char		*auto_resize_cpy(char *dst, int idx, int *buf_size, char src_ch);
 char		*get_single_env_key(char *str, int idx);
+char		*join_env_value(char *expanded_str, int *buflen, char *env_key);
+char		*auto_resize_join(char *dst, int *buflen, char ch);
 
 #endif
---
+++ src/parse/auto_resize_join.c
@@ -0,0 +1,14 @@
+#include "parse.h"
+#include "libft.h"
+
+/*
+** dstの末尾にchを一文字追加する
+*/
+char		*auto_resize_join(char *dst, int *buflen, char ch)
+{
+	int		dst_len;
+
+	dst_len = ft_strlen(dst);
+	dst = auto_resize_cpy(dst, dst_len, buflen, ch);
+	return (dst);
+}
