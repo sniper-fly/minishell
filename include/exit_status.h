691326049e4269c3821ff6786eb66439a6dad4b6
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-19 07:11:51 +0900

    exitステータスのマクロの修正

--- include/exit_status.h
+++ include/exit_status.h
@@ -4,7 +4,7 @@
 #define SUCCEEDED 0
 #define GENERAL_ERROROS 1
 #define MISUSE_OF_SHELL_BUILTINS 2	// Missing keyword or command, or permission problem
-#define CMD_CANNOT_EXECUTE 126	// Permission problem or command is not executable
+#define COMMAND_CANNOT_EXECUTE 126	// Permission problem or command is not executable
 #define COMMAND_NOT_FOUND 127	// Possible problem with $PATH or a typo
 // #define INVALID_ARG_TO_EXIT 128	// exitに不正な値(浮動小数点など)を渡した
 #define CTRL_C 130
