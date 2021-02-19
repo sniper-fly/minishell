c6ee87e409b6a3fd7b3b9f682d78de051322fd06
rnakai <rnakai@student.42tokyo.jp>
2021-02-19 12:01:55 +0900

    mallocエラー処理の目印追加

--- src/parse/auto_resize_join.c
+++ src/parse/auto_resize_join.c
@@ -9,6 +9,6 @@ char		*auto_resize_join(char *dst, int *buflen, char ch)
 	int		dst_len;
 
 	dst_len = ft_strlen(dst);
-	dst = auto_resize_cpy(dst, dst_len, buflen, ch);
+	dst = auto_resize_cpy(dst, dst_len, buflen, ch); //TODO:
 	return (dst);
 }
