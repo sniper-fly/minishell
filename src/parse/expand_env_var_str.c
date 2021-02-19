866ed0b31fb714db11321dfb8a3421010a1c175e
rnakai <rnakai@student.42tokyo.jp>
2021-02-19 23:40:28 +0900

    expandの領域外の読み取りセグフォを修正

--- src/parse/expand_env_var_str.c
+++ src/parse/expand_env_var_str.c
@@ -17,25 +17,71 @@ static t_bool	should_interpret_as_envvar(char *str, int i)
 	return (FALSE);
 }
 
-static void		handle_escape(
-	char *expanded_str, int *buflen, char *str, int *i)
+char		*handle_backslash(char *expanded_str, int *buflen, char *str, int *i)
 {
-	if (str[*i] == BACK_SLASH)
-	{
-		expanded_str = auto_resize_join(expanded_str, buflen, str[*i]);
-		expanded_str = auto_resize_join(expanded_str, buflen, str[*i + 1]);
-		*i += 2;
-		return ;
-	}
+	expanded_str = auto_resize_join(expanded_str, buflen, str[*i]);
+	expanded_str = auto_resize_join(expanded_str, buflen, str[*i + 1]);
+	*i += 2;
+	return (expanded_str);
+}
+
+static char		*handle_double_quote(char *expanded_str, int *buflen, char *str, int *i)
+{
+	char	*env_key;
+
 	expanded_str = auto_resize_join(expanded_str, buflen, str[*i]);
 	(*i)++;
-	while (str[*i] != SINGLE_QUOTE)
+	while (str[*i] != DOUBLE_QUOTE)
 	{
+		if (str[*i] == BACK_SLASH)
+		{
+			expanded_str = handle_backslash(expanded_str, buflen, str, i);
+			continue ;
+		}
+		if (should_interpret_as_envvar(str, *i))
+		{
+			(*i)++; //$をスキップする
+			env_key = get_single_env_key(str, *i);
+			expanded_str = join_env_value(expanded_str, buflen, env_key);
+			*i += ft_strlen(env_key);
+			free(env_key);
+			continue ;
+		}
 		expanded_str = auto_resize_join(expanded_str, buflen, str[*i]);
 		(*i)++;
 	}
 	expanded_str = auto_resize_join(expanded_str, buflen, str[*i]);
 	(*i)++;
+	return (expanded_str);
+}
+
+static char		*handle_escape_ch(
+	char *expanded_str, int *buflen, char *str, int *i)
+{
+	if (str[*i] == BACK_SLASH)
+	{
+		expanded_str = handle_backslash(expanded_str, buflen, str, i);
+		return (expanded_str);
+	}
+	else if (str[*i] == SINGLE_QUOTE)
+	{
+		expanded_str = auto_resize_join(expanded_str, buflen, str[*i]);
+		(*i)++;
+		while (str[*i] != SINGLE_QUOTE)
+		{
+			expanded_str = auto_resize_join(expanded_str, buflen, str[*i]);
+			(*i)++;
+		}
+		expanded_str = auto_resize_join(expanded_str, buflen, str[*i]);
+		(*i)++;
+		return (expanded_str);
+	}
+	else if (str[*i] == DOUBLE_QUOTE)
+	{
+		expanded_str = handle_double_quote(expanded_str, buflen, str, i);
+		return (expanded_str);
+	}
+	return (expanded_str);
 }
 
 char			*expand_env_var_str(char *str)
@@ -50,8 +96,11 @@ char			*expand_env_var_str(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] == SINGLE_QUOTE || str[i] == BACK_SLASH)
-			handle_escape(expanded_str, &buflen, str, &i);
+		if (str[i] == SINGLE_QUOTE || str[i] == DOUBLE_QUOTE || str[i] == BACK_SLASH)
+		{
+			expanded_str = handle_escape_ch(expanded_str, &buflen, str, &i);
+			continue ;
+		}
 		if (should_interpret_as_envvar(str, i))
 		{
 			i++; //$をスキップする
