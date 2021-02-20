08e0bd6e7857185427c915b0034f4357260f4d5a
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-20 16:06:19 +0900

    execute.hにis_builtin_funcを追加

--- include/execute.h
+++ include/execute.h
@@ -1,8 +1,10 @@
 #ifndef EXEC_CMD_H
 #define EXEC_CMD_H
 
+#include "struct/t_bool.h"
 #include "struct/process.h"
 
+t_bool is_builtin_func(char *cmd);
 void	exec_cmds(t_process *procs);
 void	my_execve(char **cmd);
 void	exec_tasks(char ***tasks);
