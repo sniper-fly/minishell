9dcf932eca58ab8138a9495a8d2eae3761d8d13f
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-20 21:11:47 +0900

    my_exportで使われる関数の引数を変更

--- include/builtins/my_export.h
+++ include/builtins/my_export.h
@@ -4,11 +4,11 @@
 #include "struct/env_list.h"
 
 void sort_keys_by_lexical_order(char **env_str_arr);
-char *create_env_str(t_env_list *env_node, int *exit_status);
+char *create_env_str(t_env_list *env_node);
 
 
 void	print_env_at_my_export(void);
-t_env_list **create_new_env_nodes_arr(char **args);
+t_env_list **create_new_env_nodes_arr(char **args, int *exit_status);
 void add_new_env_var_to_list(t_env_list *new_env_node);
 int my_export(char **args);
 
