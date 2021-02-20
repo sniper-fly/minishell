e6cb479193d7dbaa78a8cdd931acf12a7af6b260
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-20 13:35:36 +0900

    my_envのg_statusを設定

--- src/builtins/my_env.c
+++ src/builtins/my_env.c
@@ -1,9 +1,11 @@
 #include "libft.h"
 #include "utils.h"
 #include "constants.h"
+#include "exit_status.h"
 #include "struct/env_list.h"
 
 extern t_env_list *g_env_list;
+extern int g_status;
 
 static void print_env_at_my_env(void)
 {
@@ -28,4 +30,5 @@ void my_env(char **args)
 {
 	if(!ft_strcmp(args[0], "env"))
 		print_env_at_my_env();
+	g_status = 0;
 }
