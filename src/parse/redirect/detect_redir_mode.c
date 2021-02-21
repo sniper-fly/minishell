716258d1bfbc0bcd6573ab6b84969342fbf9970e
rnakai <rnakai@student.42tokyo.jp>
2021-02-21 17:34:46 +0900

    detect_redir関数の実装、テスト

---
+++ src/parse/redirect/detect_redir_mode.c
@@ -0,0 +1,79 @@
+#include "struct/process.h"
+#include "struct/redir_mode.h"
+#include "struct/t_bool.h"
+#include "parse.h"
+#include "libft.h"
+#include "utils.h"
+
+static void		init_redir_mode(t_redir_mode *redir_mode)
+{
+	redir_mode->mode_bit = 0;
+	redir_mode->fd_str = NULL;
+}
+
+static void		set_redir_fd_str(char *str_proc, int i, t_redir_mode *redir_mode)
+{
+	int		fd_len;
+
+	fd_len = 0;
+	i--;
+	//もし i - 1 が0以上で、数字だったら
+	while (i >= 0 && ft_isdigit(str_proc[i]) == TRUE)
+	{
+		fd_len++;
+		i--;
+	}
+	if (i < 0)
+	{
+		if (ft_isdigit(str_proc[0]))
+			redir_mode->fd_str = ft_substr(str_proc, 0, fd_len);
+	}
+	if (ft_isspace(str_proc[i]) && ft_isdigit(str_proc[i + 1]))
+		redir_mode->fd_str = ft_substr(str_proc, i + 1, fd_len);
+}
+
+static t_bool	is_fd_str_valid(char *fd_str)
+{
+	if (fd_str == NULL)
+		return (TRUE);
+	if (ft_strlen(fd_str) > 1)
+		return (FALSE);
+	if (fd_str[0] == '0' || fd_str[0] == '1' || fd_str[0] == '2')
+		return (TRUE);
+	return (FALSE);
+}
+
+static t_bool	is_fd_pair_valid(t_redir_mode *redir_mode)
+{
+	if (redir_mode->fd_str == NULL)
+		return (TRUE);
+	if ((redir_mode->mode_bit & REDIR_IN) && redir_mode->fd_str[0] != '0')
+		return (FALSE);
+	else if ((redir_mode->mode_bit & REDIR_OUT) && redir_mode->fd_str[0] == '0')
+		return (FALSE);
+	return (TRUE);
+}
+
+void			detect_redir_mode(char *str_proc, int i, t_redir_mode *redir_mode)
+{
+	init_redir_mode(redir_mode);
+	set_redir_fd_str(str_proc, i, redir_mode);
+	if (!is_fd_str_valid(redir_mode->fd_str))
+	{
+		redir_mode->mode_bit = REDIR_BAD_FD;
+		return ;
+	}
+	if (str_proc[i] == '<')
+		redir_mode->mode_bit = REDIR_IN;
+	else
+	{
+		if (redir_mode->fd_str != NULL && redir_mode->fd_str[0] == '2')
+			redir_mode->mode_bit = REDIR_ERR;
+		else
+			redir_mode->mode_bit = REDIR_OUT;
+		if (str_proc[i + 1] == '>')
+			redir_mode->mode_bit |= REDIR_APPEND;
+	}
+	if (!is_fd_pair_valid(redir_mode))
+		redir_mode->mode_bit = REDIR_BAD_FD;
+}
---
+++ test/cfiles/test_parse/test_redirect.c
@@ -0,0 +1,116 @@
+#ifdef DETECT_REDIR_MODE_C
+#include "struct/redir_mode.h"
+#include "parse.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+static void	test_get_redmode(char *str, t_redir_mode *redir_mode)
+{
+	int j;
+	for (j = 0; str[j] && str[j] != '<' && str[j] != '>'; j++) {;}
+	detect_redir_mode(str, j, redir_mode);
+}
+
+int		main(void)
+{
+	t_redir_mode	redir_mode;
+	char*	out1[] = {
+		">",
+		" >",
+		"a>",
+		" >>",
+	};
+	char*	out2[] = {
+		"1>a",
+		" 1>a",
+		" 1>>a",
+	};
+	char*	in1[] = {
+		"<",
+		" <",
+		"a <",
+		"a a<",
+	};
+	char*	in2[] = {
+		"0<",
+		"a 0<",
+		"a 0<",
+	};
+	char*	err1[] = {
+		"2>",
+		" 2>",
+		"a 2>",
+		"a 2>>",
+	};
+	char*	append[] = {
+		" 1>>a",
+		"a 2>>",
+		" >>",
+	};
+	char*	badfd[] = {
+		///// bad fd below
+		"112>",
+		"1<",
+		"2<",
+		"10<",
+		"3>",
+		"3>>",
+		"11>>",
+		"12>>",
+		"02>", // 本家では2として出力されるが、エラーとして処理する
+	};
+
+	for (size_t i = 0; i < sizeof(out1) / sizeof(char*); i++)
+	{
+		test_get_redmode(out1[i], &redir_mode);
+		if (!(redir_mode.mode_bit & REDIR_OUT))
+			printf("out 1 failure at %ld, %x\n", i, redir_mode.mode_bit);
+		free(redir_mode.fd_str);
+	}
+	for (size_t i = 0; i < sizeof(out2) / sizeof(char*); i++)
+	{
+		test_get_redmode(out2[i], &redir_mode);
+		if (! ((redir_mode.mode_bit & REDIR_OUT) && !strcmp(redir_mode.fd_str, "1")))
+			printf("out 2 failure at %ld\n", i);
+		free(redir_mode.fd_str);
+	}
+	for (size_t i = 0; i < sizeof(in1) / sizeof(char*); i++)
+	{
+		test_get_redmode(in1[i], &redir_mode);
+		if (! (redir_mode.mode_bit & REDIR_IN) )
+			printf("in 1 failure at %ld\n", i);
+		free(redir_mode.fd_str);
+	}
+	for (size_t i = 0; i < sizeof(in2) / sizeof(char*); i++)
+	{
+		test_get_redmode(in2[i], &redir_mode);
+		if (! ((redir_mode.mode_bit & REDIR_IN) && !strcmp(redir_mode.fd_str, "0")))
+			printf("in 2 failure at %ld\n", i);
+		free(redir_mode.fd_str);
+	}
+	for (size_t i = 0; i < sizeof(err1) / sizeof(char*); i++)
+	{
+		test_get_redmode(err1[i], &redir_mode);
+		if (! ((redir_mode.mode_bit & REDIR_ERR) && !strcmp(redir_mode.fd_str, "2")))
+			printf("err1 failure at %ld\n", i);
+		free(redir_mode.fd_str);
+	}
+	for (size_t i = 0; i < sizeof(append) / sizeof(char*); i++)
+	{
+		test_get_redmode(append[i], &redir_mode);
+		if (! (redir_mode.mode_bit & REDIR_APPEND))
+			printf("append failure at %ld\n", i);
+		free(redir_mode.fd_str);
+	}
+	for (size_t i = 0; i < sizeof(badfd) / sizeof(char*); i++)
+	{
+		test_get_redmode(badfd[i], &redir_mode);
+		printf("err msg = %s\n", redir_mode.fd_str);
+		if (! (redir_mode.mode_bit & REDIR_BAD_FD))
+			printf("badfd failure at %ld\n", i);
+		free(redir_mode.fd_str);
+	}
+}
+#endif
