f835f21944dd142d7294c3b4daa68ef842503b35
rnakai <rnakai@student.42tokyo.jp>
2021-02-22 17:17:10 +0900

    is_ambiguous_errのメモリリーク修正

--- src/parse/redirect/is_judgements.c
+++ src/parse/redirect/is_judgements.c
@@ -5,8 +5,25 @@
 #include "libft.h"
 #include "constants.h"
 
+t_bool			is_escape(char ch)
+{
+	if (ch == SINGLE_QUOTE || ch == DOUBLE_QUOTE || ch == BACK_SLASH)
+		return (TRUE);
+	return (FALSE);
+}
+
+static void		skip_escape_seq(char *trimmed, int *i)
+{
+	if (trimmed[*i] == SINGLE_QUOTE)
+		skip_until_end_single_quote(trimmed, i);
+	else if (trimmed[*i] == DOUBLE_QUOTE)
+		skip_until_end_double_quote(trimmed, i);
+	else if (trimmed[*i] == BACK_SLASH)
+		(*i) += 2;
+}
+
 //strtrimしてもなお、空白が残っている場合。エスケープあり。
-t_bool		is_ambiguous_err(char *redir_expanded)
+t_bool			is_ambiguous_err(char *redir_expanded)
 {
 	char	*trimmed;
 	int		i;
@@ -16,19 +33,15 @@ t_bool		is_ambiguous_err(char *redir_expanded)
 	while (trimmed[i])
 	{
 		if (ft_isspace(trimmed[i]))
+		{
+			free(trimmed);
 			return (TRUE);
-		if (trimmed[i] == SINGLE_QUOTE)
+		}
+		else if (is_escape(trimmed[i]))
 		{
-			skip_until_end_single_quote(trimmed, &i);
+			skip_escape_seq(trimmed, &i);
 			continue ;
 		}
-		if (trimmed[i] == DOUBLE_QUOTE)
-		{
-			skip_until_end_double_quote(trimmed, &i);
-			continue ;
-		}
-		if (trimmed[i] == BACK_SLASH)
-			i++;
 		i++;
 	}
 	free(trimmed);
