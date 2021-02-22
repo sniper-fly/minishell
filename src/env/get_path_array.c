8d994ee3c3bafc934147a42018aa30fe94cc22fe
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-22 18:13:09 +0900

    get_path_arrayのエラー処理とNorm適用

--- src/env/get_path_array.c
+++ src/env/get_path_array.c
@@ -3,20 +3,29 @@
 #include "utils.h"
 #include "struct/env_list.h"
 
-extern t_env_list *g_env_list;
+extern int			g_status;
+extern t_env_list	*g_env_list;
 
-char **get_path_array(void)
+char	**get_path_array(void)
 {
-	char **path;
-	t_env_list *tmp;
+	char		**path;
+	t_env_list	*nil;
+	t_env_list	*tmp;
 
+	nil = g_env_list;
 	tmp = g_env_list->next;
-	while(tmp->key){
-		if(!ft_strcmp(tmp->key, "PATH"))
-			break;
+	while (tmp != nil)
+	{
+		if (!ft_strcmp(tmp->key, "PATH"))
+			break ;
 		tmp = tmp->next;
 	}
-	path = ft_split(tmp->value, ':');	// TODO:エラー処理
-	return path;
+	if (tmp == g_env_list)
+		return (NULL);
+	if (!(path = ft_split(tmp->value, ':')))
+	{
+		g_status = malloc_error();
+		return (NULL);
+	}
+	return (path);
 }
-
