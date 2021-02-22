6dcf33077132021ea56fd474e414f8da28226312
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-22 13:25:08 +0900

    エラー処理によって型が変わった関数の宣言を修正(my_export.h)

--- include/builtins/my_export.h
+++ include/builtins/my_export.h
@@ -3,14 +3,12 @@
 
 #include "struct/env_list.h"
 
-void sort_keys_by_lexical_order(char **env_str_arr);
-char *create_env_str(t_env_list *env_node);
-
-
-void	print_env_at_my_export(void);
-t_env_list **create_new_env_nodes_arr(char **args, int *exit_status);
-void add_new_env_var_to_list(t_env_list *new_env_node);
-int my_export(char **args);
+int			sort_keys_by_lexical_order(char **env_str_arr);
+char 		*create_env_str(t_env_list *env_node);
+int			print_env_at_my_export(void);
+t_env_list	**create_new_env_nodes_arr(char **args, int *exit_status);
+void		add_new_env_var_to_list(t_env_list *new_env_node);
+int			my_export(char **args);
 
 
 #endif
