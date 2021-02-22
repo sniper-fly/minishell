7420ff08d0efe0ee61f61703a62fb0a1ad6e6f07
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-22 13:26:25 +0900

    my_pwdにエラー処理を追加

--- src/builtins/my_pwd.c
+++ src/builtins/my_pwd.c
@@ -1,15 +1,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "libft.h"
+#include "utils.h"
 #include "constants.h"
 #include "exit_status.h"
 
-int my_pwd(char **args)
+int	my_pwd(char **args)
 {
-	char *pwd;
+	char	*pwd;
 
 	(void)args;
-	pwd = getcwd(NULL, 0);	// note:エラー処理
+	if (!(pwd = getcwd(NULL, 0)))
+	{
+		ft_putstr_fd("minishell: pwd: ", STD_ERR);
+		ft_perror(NULL);
+	}
 	ft_putendl_fd(pwd, STD_OUT);
 	free(pwd);
 	return (SUCCEEDED);
