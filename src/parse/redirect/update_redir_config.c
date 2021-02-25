f7fc1aa15aef31b9a4f69392218a256e445abb0e
rnakai <rnakai@student.42tokyo.jp>
2021-02-25 13:02:17 +0900

    redir_configに代入するときにfree忘れを修正

--- src/parse/redirect/update_redir_config.c
+++ src/parse/redirect/update_redir_config.c
@@ -1,6 +1,7 @@
 #include "struct/redir_mode.h"
 #include "struct/process.h"
 #include <fcntl.h>
+#include <stdlib.h>
 
 void		update_redir_config(
 	t_process *redir_config, char *redir_filename, t_redir_mode *redir_mode)
@@ -9,6 +10,7 @@ void		update_redir_config(
 
 	if (redir_mode->mode_bit & REDIR_IN)
 	{
+		free(redir_config->red_in_file_name);
 		redir_config->red_in_file_name = redir_filename;
 		return ;
 	}
@@ -18,11 +20,13 @@ void		update_redir_config(
 		red_flag_bit = O_TRUNC | O_CREAT | O_RDWR;
 	if (redir_mode->mode_bit & REDIR_OUT)
 	{
+		free(redir_config->red_out_file_name);
 		redir_config->red_out_file_name = redir_filename;
 		redir_config->red_out_mode = red_flag_bit;
 	}
 	else if (redir_mode->mode_bit & REDIR_ERR)
 	{
+		free(redir_config->red_err_file_name);
 		redir_config->red_err_file_name = redir_filename;
 		redir_config->red_err_mode = red_flag_bit;
 	}
