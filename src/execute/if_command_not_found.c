2b145ce58cb0170f82451939d6e58c5eab8b6271
yyabumot <yyabumot@student.42tokyo.jp>
2021-02-27 14:12:04 +0900

    if_command_not_found関数を別ファイルに移動

---
+++ src/execute/if_command_not_found.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "libft.h"
+#include "constants.h"
+#include "exit_status.h"
+
+void	if_command_not_found(char *cmd_path)
+{
+	ft_putstr_fd(cmd_path, STD_ERR);
+	ft_putstr_fd(": ", STD_ERR);
+	ft_putendl_fd("command not found", STD_ERR);
+	exit(COMMAND_NOT_FOUND);
+}
