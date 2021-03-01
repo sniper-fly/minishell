6104bd186025c8f1189cbc1ec1b9feae4e0f8137
yyabumot <yyabumot@student.42tokyo.jp>
2021-03-01 11:55:38 +0900

    PWDを出力するように修正

--- src/builtins/my_pwd.c
+++ src/builtins/my_pwd.c
@@ -2,21 +2,30 @@
 #include <unistd.h>
 #include "libft.h"
 #include "utils.h"
+#include "env_ctrl.h"
 #include "constants.h"
 #include "exit_status.h"
 
+extern t_env_list *g_env_list;
+
 int	my_pwd(char **args)
 {
-	char	*pwd;
+	char		*pwd;
+	t_env_list	*pwd_node;
 
 	(void)args;
-	if (!(pwd = getcwd(NULL, 0)))
+	if ((pwd_node = search_env_node("PWD")) != g_env_list)
+		ft_putendl_fd(pwd_node->value, STD_OUT);
+	else
 	{
-		ft_putstr_fd("minishell: pwd: ", STD_ERR);
-		ft_perror(NULL);
-		return (GENERAL_ERRORS);
+		if (!(pwd = getcwd(NULL, 0)))
+		{
+			ft_putstr_fd("minishell: pwd: ", STD_ERR);
+			ft_perror(NULL);
+			return (GENERAL_ERRORS);
+		}
+		ft_putendl_fd(pwd, STD_OUT);
+		free(pwd);
 	}
-	ft_putendl_fd(pwd, STD_OUT);
-	free(pwd);
 	return (SUCCEEDED);
 }
