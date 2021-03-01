ad1eef9d3cb0ce0244827f94bca6c6c8a0eb9808
yyabumot <yyabumot@student.42tokyo.jp>
2021-03-02 07:03:59 +0900

    my_pwdのテストの修正

--- test/cfiles/test_builtins.c
+++ test/cfiles/test_builtins.c
@@ -124,6 +124,7 @@ int main(void)
 #ifdef MY_PWD_C
 
 #include "libft.h"
+#include "main.h"
 #include "env_ctrl.h"
 #include "struct/env_list.h"
 
@@ -138,6 +139,7 @@ int main(int argc, char *argv[], char *envp[])
 	argv[0][0] = 'a';
 
 	create_env_list(envp);
+	init_pwd();
 
 	my_pwd(pwd_args);
 	my_cd(cd_args);
