1f56b35fabd268ae45bb7894bbf204935bb10217
yyabumot <yyabumot@student.42tokyo.jp>
2021-03-01 13:11:43 +0900

    my_cdにカレントディレクトリが消されたときの処理を追加

--- src/builtins/my_cd.c
+++ src/builtins/my_cd.c
@@ -16,30 +16,70 @@ static void	put_error_msg(char *file_path)
 	ft_perror(file_path);
 }
 
-static int	change_env_vars(void)
+static int	set_pwd(char **args_for_my_export)
 {
 	char		*pwd;
-	char		**args_to_my_export;
 	t_env_list	*pwd_node;
 
-	if (!(args_to_my_export = malloc(sizeof(char *) * 4)))
+	if (!(pwd = getcwd(NULL, 0)) && errno == ENOENT)
+	{
+		put_error_msg(NULL);
+		pwd_node = search_env_node("PWD");	// note:if no exist
+		if (pwd_node->value[ft_strlen(pwd_node->value) - 1] == '/')
+		{
+			if (!(pwd = ft_strjoin(pwd_node->value, ".")))
+				return (ERROR);
+		}
+		else
+		{
+			if (!(pwd = ft_strjoin(pwd_node->value, "/.")))
+				return (ERROR);
+		}
+	}
+	if (!(args_for_my_export[1] = ft_strjoin("PWD=", pwd)))
+	{
+		free_string_arr(args_for_my_export);
 		return (ERROR);
-	if (!(args_to_my_export[0] = ft_strdup("export")))
-		return (ERROR);
-	pwd = getcwd(NULL, 0);
-	if (!(args_to_my_export[1] = ft_strjoin("PWD=", pwd)))
-		return (ERROR);
+	}
+	free(pwd);
+	return (SUCCEEDED);
+}
+
+static int	set_old_pwd(char **args_for_my_export)
+{
+	t_env_list	*pwd_node;
+
 	if ((pwd_node = search_env_node("PWD")) != g_env_list)
 	{
-		if (!(args_to_my_export[2] = ft_strjoin("OLDPWD=", pwd_node->value)))
+		if (!(args_for_my_export[2] = ft_strjoin("OLDPWD=", pwd_node->value)))
 			return (ERROR);
 	}
 	else
-		args_to_my_export[2] = NULL;
-	args_to_my_export[3] = NULL;
-	my_export(args_to_my_export);
-	free(pwd);
-	free_string_arr(args_to_my_export);
+		args_for_my_export[2] = NULL;
+	return (SUCCEEDED);
+}
+
+
+static int	change_env_vars(void)
+{
+	char		**args_for_my_export;
+
+	if (!(args_for_my_export = malloc(sizeof(char *) * 4)))
+		return (ERROR);
+	if (!(args_for_my_export[0] = ft_strdup("export")))
+	{
+		free_string_arr(args_for_my_export);
+		return (ERROR);
+	}
+	if (set_pwd(args_for_my_export) == ERROR
+	|| set_old_pwd(args_for_my_export) == ERROR)
+	{
+		free_string_arr(args_for_my_export);
+		return (ERROR);
+	}
+	args_for_my_export[3] = NULL;
+	my_export(args_for_my_export);
+	free_string_arr(args_for_my_export);
 	return (0);
 }
 
