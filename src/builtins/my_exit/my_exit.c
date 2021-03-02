b035ff7c53f5a0de27e5b50b0fcfb2ff6b8d4e93
yyabumot <yyabumot@student.42tokyo.jp>
2021-03-02 16:14:52 +0900

    my_exitのテストケースを追加

--- src/builtins/my_exit/my_exit.c
+++ src/builtins/my_exit/my_exit.c
@@ -3,30 +3,37 @@
 #include "utils.h"
 #include "constants.h"
 #include "exit_status.h"
+#include "struct/t_bool.h"
 #include "builtins/my_exit.h"
 #include <signal.h>
 
 extern volatile sig_atomic_t g_status;
 
-static int		count_digit(char *status)
+static int		is_numeric_argument(char *arg)
 {
+	int	i;
 	int digit;
 
+	i = 0;
 	digit = 0;
-	if (*status == '-')
-		++status;
-	while (*status)
+	if (arg[i] == '-' || arg[i] == '+')
+		++i;
+	while (arg[i])
 	{
+		if (arg[i] < '0' || '9' < arg[i])
+			return (FALSE);
 		++digit;
-		++status;
+		++i;
 	}
-	return (digit);
+	if (20 < digit)
+		return (FALSE);
+	return (TRUE);
 }
 
 int				my_exit(char **args)
 {
 	ft_putendl_fd("exit", STD_ERR);
-	if (args[1] && 20 <= count_digit(args[1]))
+	if (args[1] && !is_numeric_argument(args[1]))
 	{
 		ft_putstr_fd("minishell: exit: ", STD_ERR);
 		ft_putstr_fd(args[1], STD_ERR);
