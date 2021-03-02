c25f30b0cce57f7c02fa14a1fda273bbe2f7778d
yyabumot <yyabumot@student.42tokyo.jp>
2021-03-03 07:43:34 +0900

    command not foundの時にg_statusを127に設定するように修正

--- src/execute/if_command_not_found.c
+++ src/execute/if_command_not_found.c
@@ -3,11 +3,14 @@
 #include "constants.h"
 #include "exit_status.h"
 
+extern int	g_status;
+
 void	if_command_not_found(char *cmd_path)
 {
 	ft_putstr_fd("minishell: ", STD_ERR);
 	ft_putstr_fd(cmd_path, STD_ERR);
 	ft_putstr_fd(": ", STD_ERR);
 	ft_putendl_fd("command not found", STD_ERR);
+	g_status = COMMAND_NOT_FOUND;
 	exit(COMMAND_NOT_FOUND);
 }
