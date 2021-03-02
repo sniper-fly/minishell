e33e395feae919abb2bf29a4e8dd69ee0c251145
yyabumot <yyabumot@student.42tokyo.jp>
2021-03-02 16:48:25 +0900

    エラー処理を修正

--- src/builtins/my_exit/my_exit.c
+++ src/builtins/my_exit/my_exit.c
@@ -32,20 +32,27 @@ static int		is_numeric_argument(char *arg)
 
 int				my_exit(char **args)
 {
+	int	i;
+
 	ft_putendl_fd("exit", STD_ERR);
-	if (args[1] && !is_numeric_argument(args[1]))
+	i = 1;
+	while(args[i])
 	{
-		ft_putstr_fd("minishell: exit: ", STD_ERR);
-		ft_putstr_fd(args[1], STD_ERR);
-		ft_putendl_fd(": numeric argument required", STD_ERR);
-		g_status = MISUSE_OF_SHELL_BUILTINS;
-		exit(MISUSE_OF_SHELL_BUILTINS);
-	}
-	if (2 < count_string_arr_row(args))
-	{
-		ft_putendl_fd("minishell: exit: too many arguments\n", STD_ERR);
-		g_status = GENERAL_ERRORS;
-		return (GENERAL_ERRORS);
+		if (1 < i)
+		{
+			ft_putendl_fd("minishell: exit: too many arguments\n", STD_ERR);
+			g_status = GENERAL_ERRORS;
+			return (GENERAL_ERRORS);
+		}
+		if (!is_numeric_argument(args[i]))
+		{
+			ft_putstr_fd("minishell: exit: ", STD_ERR);
+			ft_putstr_fd(args[i], STD_ERR);
+			ft_putendl_fd(": numeric argument required", STD_ERR);
+			g_status = MISUSE_OF_SHELL_BUILTINS;
+			exit(MISUSE_OF_SHELL_BUILTINS);
+		}
+		++i;
 	}
 	if (args[1])
 		g_status = ft_atol(args[1]) & 255;
