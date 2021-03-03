18abef76fb55f41051150b3975a44208d6f836d3
yyabumot <yyabumot@student.42tokyo.jp>
2021-03-03 19:33:47 +0900

    コマンドラインの行末を弾くように修正

--- src/validation/is_valid_command_line.c
+++ src/validation/is_valid_command_line.c
@@ -1,8 +1,15 @@
+#include "libft.h"
 #include "validation.h"
 #include "struct/t_bool.h"
 
+#include <stdio.h>
 t_bool	is_valid_command_line(char *cmd_line)
 {
+	if (cmd_line[ft_strlen(cmd_line) - 2] == '\\')
+	{
+		put_syntax_error_message("multiple command line is forbidden");
+		return (FALSE);
+	}
 	if (!is_valid_char_code(cmd_line))
 		return (FALSE);
 	if (!is_valid_meta_and_redirect(cmd_line))
