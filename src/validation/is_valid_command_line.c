1603e325ab4125d47eab2eac4178299e12958c71
yyabumot <yyabumot@student.42tokyo.jp>
2021-03-03 23:00:27 +0900

    エラーの条件を修正

--- src/validation/is_valid_command_line.c
+++ src/validation/is_valid_command_line.c
@@ -2,14 +2,31 @@
 #include "validation.h"
 #include "struct/t_bool.h"
 
-#include <stdio.h>
+static t_bool	is_cmd_line_terminated_by_bkslsh(char *cmd_line)
+{
+	if (2 <= ft_strlen(cmd_line))
+	{
+		if (cmd_line[ft_strlen(cmd_line) - 2] == '\\')
+		{
+			if (ft_strlen(cmd_line) == 2)
+			{
+				put_syntax_error_message("multiple command line is forbidden");
+				return (TRUE);
+			}
+			if (cmd_line[ft_strlen(cmd_line) - 3] != '\\')
+			{
+				put_syntax_error_message("multiple command line is forbidden");
+				return (TRUE);
+			}
+		}
+	}
+	return (FALSE);
+}
+
 t_bool	is_valid_command_line(char *cmd_line)
 {
-	if (cmd_line[ft_strlen(cmd_line) - 2] == '\\')
-	{
-		put_syntax_error_message("multiple command line is forbidden");
+	if (is_cmd_line_terminated_by_bkslsh(cmd_line))
 		return (FALSE);
-	}
 	if (!is_valid_char_code(cmd_line))
 		return (FALSE);
 	if (!is_valid_meta_and_redirect(cmd_line))
