a8d4272b43bafabc08fbea85e61babc135b418ac
yyabumot <yyabumot@student.42tokyo.jp>
2021-03-03 10:37:37 +0900

    my_cdで使用する関数でmallocエラーが起きたらすぐexitするように修正

--- include/builtins/my_cd.h
+++ include/builtins/my_cd.h
@@ -1,7 +1,7 @@
 #ifndef MY_CD_H
 # define MY_CD_H
 
-int		change_env_vars(void);
+void	change_env_vars(void);
 void	put_error_msg(char *file_path);
 
 #endif
--- src/builtins/my_cd/my_cd.c
+++ src/builtins/my_cd/my_cd.c
@@ -21,12 +21,12 @@ static char		*replace_homedir_with_path(char *arg)
 	if (!arg)
 	{
 		if (!(path = ft_strdup(homedir_path)))
-			return (NULL);
+			exit(malloc_error());
 	}
 	else
 	{
 		if (!(path = ft_strjoin(homedir_path, &arg[1])))
-			return (NULL);
+			exit(malloc_error());
 	}
 	return (path);
 }
@@ -36,14 +36,11 @@ static char		*set_path(char *arg)
 	char	*path;
 
 	if (!arg || arg[0] == '~')
-	{
-		if (!(path = replace_homedir_with_path(arg)))
-			return (NULL);
-	}
+		path = replace_homedir_with_path(arg);
 	else
 	{
 		if (!(path = ft_strdup(arg)))
-			return (NULL);
+			exit(malloc_error());
 	}
 	return (path);
 }
@@ -70,8 +67,7 @@ int				my_cd(char **args)
 
 	if (!is_valid_arg(args))
 		return (GENERAL_ERRORS);
-	if (!(path = set_path(args[1])))
-		return (malloc_error());
+	path = set_path(args[1]);
 	if (chdir(path) == -1)
 	{
 		put_error_msg(path);
@@ -79,10 +75,9 @@ int				my_cd(char **args)
 		return (GENERAL_ERRORS);
 	}
 	free(path);
-	if (change_env_vars() == ERROR)
-		return (malloc_error());
+	change_env_vars();
 	if (!(new_pwd = ft_strdup(search_env_node("PWD")->value)))
-		return (malloc_error());
+		exit(malloc_error());
 	free(g_pwd);
 	g_pwd = new_pwd;
 	return (SUCCEEDED);
