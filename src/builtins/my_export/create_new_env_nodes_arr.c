a5f267ad706d1e681617d4bbfae4f5f082f2e9dd
yyabumot <yyabumot@student.42tokyo.jp>
2021-03-03 15:59:09 +0900

    正当でない引数をスキップするように修正

--- src/builtins/my_export/create_new_env_nodes_arr.c
+++ src/builtins/my_export/create_new_env_nodes_arr.c
@@ -11,22 +11,27 @@ extern t_env_list *g_env_list;
 t_env_list	**create_new_env_nodes_arr(char **args, int *exit_status)
 {
 	int			i;
+	int			j;
 	t_env_list	**new_env_nodes;
 
 	if (!(new_env_nodes =
 	malloc(sizeof(t_env_list *) * (count_string_arr_row(args)))))
 		exit(malloc_error());
 	i = 0;
-	while (args[i + 1])
+	j = 1;
+	while (args[j])
 	{
-		if (!(new_env_nodes[i] = make_new_env_node(args[i + 1])))
+		if (!(new_env_nodes[i] = make_new_env_node(args[j])))
 		{
 			ft_putstr_fd("minishell: export: \'", STD_ERR);
-			ft_putstr_fd(args[i + 1], STD_ERR);
+			ft_putstr_fd(args[j], STD_ERR);
 			ft_putstr_fd("\': not a valid identifier\n", STD_ERR);
 			*exit_status = GENERAL_ERRORS;
+			++j;
+			continue ;
 		}
 		++i;
+		++j;
 	}
 	new_env_nodes[i] = NULL;
 	return (new_env_nodes);
