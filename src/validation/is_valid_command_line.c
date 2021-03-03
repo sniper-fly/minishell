ac916d323dc46b1c9c832bb669c4fa7fe9e3c298
rnakai <rnakai@student.42tokyo.jp>
2021-03-04 00:11:27 +0900

    バックスラッシュ終端を検知するvalidate関数を作成

--- src/validation/is_valid_command_line.c
+++ src/validation/is_valid_command_line.c
@@ -1,8 +1,34 @@
 #include "validation.h"
 #include "struct/t_bool.h"
+#include "constants.h"
+
+static t_bool	is_cmd_line_terminated_with_bkslsh(char *cmd_line)
+{
+	int		i;
+
+	i = 0;
+	while (cmd_line[i])
+	{
+		if (cmd_line[i] == BACK_SLASH)
+		{
+			if (cmd_line[i + 1] == '\n')
+			{
+				put_syntax_error_message(
+					"terminating with backslash is forbidden.");
+				return (TRUE);
+			}
+			i += 2;
+			continue ;
+		}
+		i++;
+	}
+	return (FALSE);
+}
 
 t_bool	is_valid_command_line(char *cmd_line)
 {
+	if (is_cmd_line_terminated_with_bkslsh(cmd_line))
+		return (FALSE);
 	if (!is_valid_char_code(cmd_line))
 		return (FALSE);
 	if (!is_valid_meta_and_redirect(cmd_line))
