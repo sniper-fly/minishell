748c1e9eec4f861aa7c4edd9d2ba6df2544a15ba
rnakai <rnakai@student.42tokyo.jp>
2021-03-04 00:26:43 +0900

    is_valid_command_line.cからバックスラッシュ終端判定関数を分離

--- src/validation/is_valid_command_line.c
+++ src/validation/is_valid_command_line.c
@@ -2,29 +2,6 @@
 #include "struct/t_bool.h"
 #include "constants.h"
 
-static t_bool	is_cmd_line_terminated_with_bkslsh(char *cmd_line)
-{
-	int		i;
-
-	i = 0;
-	while (cmd_line[i])
-	{
-		if (cmd_line[i] == BACK_SLASH)
-		{
-			if (cmd_line[i + 1] == '\n')
-			{
-				put_syntax_error_message(
-					"terminating with backslash is forbidden.");
-				return (TRUE);
-			}
-			i += 2;
-			continue ;
-		}
-		i++;
-	}
-	return (FALSE);
-}
-
 t_bool	is_valid_command_line(char *cmd_line)
 {
 	if (is_cmd_line_terminated_with_bkslsh(cmd_line))
