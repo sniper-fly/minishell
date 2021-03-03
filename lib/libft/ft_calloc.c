19c8e1a5e67e6a143c7782683f65f0e6ace3954b
rnakai <rnakai@student.42tokyo.jp>
2021-03-03 12:51:12 +0900

    callocでmallocエラーが起きたらexitするようにライブラリを変更

--- lib/libft/ft_calloc.c
+++ lib/libft/ft_calloc.c
@@ -6,13 +6,19 @@
 /*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/02 17:11:35 by rnakai            #+#    #+#             */
-/*   Updated: 2020/07/02 17:27:04 by rnakai           ###   ########.fr       */
+/*   Updated: 2021/03/03 12:35:09 by rnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <stdlib.h>
 #include "libft.h"
+
+static int	memalloc_error(void)
+{
+	ft_putendl_fd("minishell: malloc error", 2);
+	return (2);
+}
 
 void		*ft_calloc(size_t nmemb, size_t size)
 {
@@ -21,7 +27,7 @@ void		*ft_calloc(size_t nmemb, size_t size)
 	ptr = (void *)malloc(nmemb * size);
 	if (ptr == NULL)
 	{
-		return (ptr);
+		exit(memalloc_error());
 	}
 	ft_bzero(ptr, nmemb * size);
 	return (ptr);
