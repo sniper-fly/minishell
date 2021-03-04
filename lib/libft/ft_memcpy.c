984f6c5e40d6620187df6659614b2f46cb3d4da4
yyabumot <yyabumot@student.42tokyo.jp>
2021-03-04 14:03:34 +0900

    ft_memcpy.cにNorm適用

--- lib/libft/ft_memcpy.c
+++ lib/libft/ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: yyabumot <yyabumot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/29 18:21:05 by rnakai            #+#    #+#             */
-/*   Updated: 2020/07/14 17:16:35 by rnakai           ###   ########.fr       */
+/*   Updated: 2021/03/04 14:02:55 by yyabumot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,15 +28,3 @@ void		*ft_memcpy(void *dst, const void *src, size_t len)
 	}
 	return (dst);
 }
-
-// #include <stdio.h>
-// #include <string.h>
-// int		main(void)
-// {
-// 	char *	hoge = NULL;
-// 	char *ahoge = NULL;
-// 	// printf("%p", memcpy(hoge, ahoge, 0));
-// 	// printf("%p", ft_memcpy(hoge, ahoge, 0));
-// 	printf("\n");
-// 	return (0);
-// }
